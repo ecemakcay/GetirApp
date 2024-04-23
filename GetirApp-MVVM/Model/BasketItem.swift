6ae941f0ccf68d190ed7f2eae083f19c41484ba3
Ecem Akçay <ecemakcay00@gmail.com>
2024-04-24 01:41:09 +0300

    BasketItem added

---
+++ GetirApp-MVVM/Model/BasketItem.swift
@@ -0,0 +1,17 @@
+//
+//  BasketItem.swift
+//  GetirApp-MVVM
+//
+//  Created by Ecem Akçay on 23.04.2024.
+//
+
+import Foundation
+
+struct BasketItem {
+    let name: String?
+    let attribute: String?
+    let price: Double?
+    var quantity: Int
+    let id: String?
+    let imageURL: String?
+}
